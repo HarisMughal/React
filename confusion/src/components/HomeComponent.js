7edbfe76c8b9307fdb67a109191b392221203e6d
HarisMughal <hsmughal4@gmail.com>
2020-04-20 21:06:27 +0500

    Contact and home page updated

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 
+
+
+function RenderCard({dish}){
+    console.log(dish);
+    return (<Card>
+        <CardImg src={dish.image}></CardImg>
+        <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            {dish.description ? <CardSubtitle>{dish.designation}</CardSubtitle> : null}
+            <CardText>{dish.description}</CardText>
+        </CardBody>
+    </Card>);
+}
 
 function Home(props){
     return (
         <div className="container">
-            <h1>Home Page</h1>
+            <div className="row align-item-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard dish={props.dish}/>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard dish={props.promotion}/>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard dish={props.leader}/>
+                </div>
+            </div>
         </div>
     );
 }
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,10 +8,14 @@ import React,{Component} from 'react';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotion';
+import {LEADERS} from '../shared/leaders';
 
 
 
@@ -32,6 +36,9 @@ class Main extends Component{
     this.state = 
      {
        dishes:DISHES,
+       comments:COMMENTS,
+       promotion:PROMOTIONS,
+       leaders:LEADERS
       //  selectedDishId: null
 
     };
@@ -56,7 +63,14 @@ class Main extends Component{
 // Menu and Dish has been changed to be a funtional component ( as such components are only used to render only view
 // and does not require data to be feeded )
     const HomePage = ()=> { // The only way to pass a component in Route we have to use funtional conponent 
-       return (<Home/>);
+      
+       return (
+        <Home dish= { this.state.dishes.filter( (dish)=> dish.featured  )[0]}
+          promotion = { this.state.promotion.filter( (promotion)=> promotion.featured  )[0]}
+          leader = { this.state.leaders.filter( (leaders)=> leaders.featured  )[0]}
+          // test = { console.log(this.state.leaders.filter( (leaders)=> leaders.featured  ) )}
+        />
+       );
     }
     // below is an another way of sending component as funtional component
     return (
@@ -65,6 +79,7 @@ class Main extends Component{
         <Switch>
           <Route path="/home" component = {HomePage} />
           <Route exact path="/menu" component = { ()=> <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path="/contact" component = {Contact}/>}/>
           <Redirect to = "/home" ></Redirect>
         </Switch>
         {/* <Menu dishes={this.state.dishes}
