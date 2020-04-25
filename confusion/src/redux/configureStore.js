09f5c80d18f99e51fad134872324051c0b5d4402
HarisMughal <hsmughal4@gmail.com>
2020-04-25 17:52:11 +0500

    Combined reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,10 +1,17 @@
-import {Reducer, initialState } from "./reducer";
-import {createStore} from 'redux';
+import {createStore,combineReducers} from 'redux';
+import {Dishes} from './dishes';
+import {Comments} from './comments';
+import {Leaders} from './leaders';
+import {Promotions} from './promotions';
 
 export const ConfigureStore = ()=>{
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes:Dishes,
+            leaders:Leaders,
+            promotion:Promotions,
+            comments:Comments
+        })
     );
     return store;
 }
